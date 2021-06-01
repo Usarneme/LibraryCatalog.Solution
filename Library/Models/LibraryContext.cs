e2b20daeec0e5a6410a028c30e4dea9563c8acaf
Usarneme <jamestcoop@gmail.com>
2021-06-01 11:09:28 -0700

    :construction: adds boilerplate for mvc w/auth app
    Co-authored-by: Jeremy Banka <hello@jeremybanka.com>

---
+++ Library/Models/LibraryContext.cs
@@ -0,0 +1,15 @@
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Library.Models
+{
+  public class LibraryContext : IdentityDbContext<ApplicationUser>
+  {
+    public virtual DbSet<Book> Books { get; set; }
+    public LibraryContext(DbContextOptions options) : base(options) { }
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+      optionsBuilder.UseLazyLoadingProxies();
+    }
+  }
+}
