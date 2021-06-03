78755bf8bd40e8bc71761f362c8ec8f7c67ad101
Jeremy Banka <hello@jeremybanka.com>
2021-06-03 13:14:12 -0700

    🗃️  migrate db: user contains checkouts
    Co-authored-by: Tom/Usarneme <jamestcoop@gmail.com>

--- Library/Models/LibraryUser.cs
+++ Library/Models/LibraryUser.cs
@@ -1,12 +1,16 @@
 using Microsoft.AspNetCore.Identity;
+using System.Collections.Generic;
 
 namespace Library.Models
 {
   public class LibraryUser : IdentityUser
   {
-    public LibraryUser() : base() {
+    public LibraryUser() : base()
+    {
       IsLibrarian = false;
+      Checkouts = new HashSet<Checkout>();
     }
     public bool IsLibrarian { get; set; }
+    public virtual ICollection<Checkout> Checkouts { get; }
   }
 }
